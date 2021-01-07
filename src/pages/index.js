dd87ab3b69d7abc2588420a1284f6af272da5853
Renddi Yeh <renddi@relab.cc>
2021-01-07 17:59:03 +0800

    updated title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
 import Page from '../containers/HomePage'
 
-export default Page
+const Render = () => {
+  return (
+    <>
+      <Helmet>
+        <title>101個台灣人必懂的垃圾</title>
+      </Helmet>
+      <Page />
+    </>
+  )
+}
+
+export default Render
