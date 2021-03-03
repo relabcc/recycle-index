3e3a62e3630389d70a387c8b4c377aed59879a2f
Renddi Yeh <renddi@relab.cc>
2021-03-03 19:15:48 +0800

    Updates

---
+++ component---src-pages-game-share-3-js-c5a7e7f5c92cf5b05afc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{w7xT:function(t,e,n){"use strict";n.r(e);var a=n("Wbzz"),c=n("q1tI"),u=n.n(c),i=n("qhky"),b=n("AeFk");e.default=function(t){var e=t.data;return Object(c.useEffect)((function(){Object(a.d)("/game")}),[]),Object(b.c)(u.a.Fragment,null,Object(b.c)(i.a,null,Object(b.c)("title",null,"丟垃圾大考驗"),Object(b.c)("meta",{name:"og:image",content:e.site.siteMetadata.url+"/game/3.jpg"})))}}}]);
+//# sourceMappingURL=component---src-pages-game-share-3-js-c5a7e7f5c92cf5b05afc.js.map
