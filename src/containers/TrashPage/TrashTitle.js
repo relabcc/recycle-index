0e1b4f0f0c9574bb1f4058544fae8421de4b1d96
Renddi Yeh <renddi@relab.cc>
2021-08-06 18:32:42 +0800

    trashtitle nowrap

--- src/containers/TrashPage/TrashTitle.js
+++ src/containers/TrashPage/TrashTitle.js
@@ -7,7 +7,7 @@ import { responsive } from '../../components/ThemeProvider/theme'
 
 const TrashTitle = ({ data, color }) => {
   const [measureRef, { width }] = useMeasure()
-  const fontVwSize = useMemo(() => `${round(100 / (data.name.length + (data.name.length > 2 ? 0.5 : 1.375)), 1)}vw`, [data.name])
+  const fontVwSize = useMemo(() => `${round(100 / (data.name.length + (data.name.length > 2 ? 0.5 : 1.25)), 1)}vw`, [data.name])
   const fontSize = useMemo(() => width ? `${Math.min(Math.floor(width / (data.name.length + 1)), width / 4.5)}px` : fontVwSize, [data.name, width, fontVwSize])
   return (
     <div ref={measureRef}>
@@ -18,6 +18,7 @@ const TrashTitle = ({ data, color }) => {
         fontWeight="900"
         letterSpacing={data.name.length < 3 && '0.5em'}
         ml={data.name.length < 3 && '0.25em'}
+        whiteSpace="nowrap"
       >
         {data.name}
       </Text>
