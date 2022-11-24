ef1ddf86fb273a4555069a453ef763066e9a9903
Renddi Yeh <renddi@relab.cc>
2022-11-24 18:30:41 +0800

    removed blog

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -22,7 +22,7 @@ const links = [
   { name: '丟垃圾大考驗', en: 'Recycle Challenge', to: '/game/' },
   { name: '必懂的回收知識', en: 'What Happened After Recycling', to: '/how/' },
   { name: '關於我們', en: 'About Us', to: '/about/' },
-  { name: '文章專區', en: 'Articles', href: '/blog/' },
+  // { name: '文章專區', en: 'Articles', href: '/blog/' },
   { name: '課程申請', href: 'https://rethinktw.cc/QFrtL', hideEn: true, isExternal: true },
   { name: '贊助我們', href: 'https://rethinktw.neticrm.tw/civicrm/contribute/transact?reset=1&id=26', isSupport: true, hideEn: true, isExternal: true },
 ]
