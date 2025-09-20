e59fa67c3ef803859df71f485de42afa6be09eec
Renddi Yeh <renddi@relab.cc>
2025-09-20 14:08:26 +0800

    fix: 修正 茶葉蛋殼加塑膠袋

--- src/images/茶葉蛋殼加塑膠袋/index.png.js
+++ src/images/茶葉蛋殼加塑膠袋/index.png.js
@@ -3,5 +3,5 @@
     module.exports = {
       '塑膠袋': path.resolve(__dirname, './茶葉蛋殼加塑膠袋-塑膠袋.png'),
 '蛋殼': path.resolve(__dirname, './茶葉蛋殼加塑膠袋-蛋殼.png'),
-'茶葉蛋殼夾塑膠袋': path.resolve(__dirname, './茶葉蛋殼夾塑膠袋.png'),
+'茶葉蛋殼加塑膠袋': path.resolve(__dirname, './茶葉蛋殼加塑膠袋.png'),
     }
