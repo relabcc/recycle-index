ada39ea05ca6a3aa97ef301314f8f8a43ccf1caa
Renddi Yeh <renddi@relab.cc>
2025-09-20 13:48:50 +0800

    feat: 更新既有品項

--- src/images/生滾皮蛋煲粥/index.png.js
+++ src/images/生滾皮蛋煲粥/index.png.js
@@ -1,9 +1,9 @@
 
     const path = require('path')
     module.exports = {
-      '塑膠膜': path.resolve(__dirname, './生滾皮蛋粥-塑膠膜.png'),
-'塑膠蓋': path.resolve(__dirname, './生滾皮蛋粥-塑膠蓋.png'),
-'湯碗': path.resolve(__dirname, './生滾皮蛋粥-湯碗.png'),
-'紙板': path.resolve(__dirname, './生滾皮蛋粥-紙板.png'),
-'生滾皮蛋粥': path.resolve(__dirname, './生滾皮蛋粥.png'),
+      '塑膠膜': path.resolve(__dirname, './生滾皮蛋煲粥-塑膠膜.png'),
+'塑膠蓋': path.resolve(__dirname, './生滾皮蛋煲粥-塑膠蓋.png'),
+'湯碗': path.resolve(__dirname, './生滾皮蛋煲粥-湯碗.png'),
+'紙板': path.resolve(__dirname, './生滾皮蛋煲粥-紙板.png'),
+'生滾皮蛋煲粥': path.resolve(__dirname, './生滾皮蛋煲粥.png'),
     }
