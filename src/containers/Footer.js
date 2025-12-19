cf9e80dba55f177aff55671e2f81970302b1c5f3
Renddi Yeh <renddi@relab.cc>
2025-12-19 12:32:42 +0800

    feat: enhance footer design with responsive layout and subscription form

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,25 +1,7 @@
-import React from "react";
-import {
-  Flex,
-  Stack,
-  SimpleGrid,
-  Icon,
-  useDisclosure,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalCloseButton,
-} from "@chakra-ui/react";
-import {
-  FiFacebook,
-  FiInstagram,
-  FiMail,
-  FiMapPin,
-  FiPhone,
-  FiExternalLink,
-} from "react-icons/fi";
+import React, { useState } from "react";
+import { Flex, Stack, SimpleGrid, Icon, Input, Image } from "@chakra-ui/react";
+import { FiFacebook, FiInstagram, FiYoutube } from "react-icons/fi";
+import { SiThreads, SiLine } from "react-icons/si";
 
 import Box from "../components/Box";
 import Container from "../components/Container";
@@ -29,47 +11,64 @@ import Sponsor from "../components/Sponsor";
 import { responsive } from "../components/ThemeProvider/theme";
 
 const Footer = ({ isAbout, isTrash, noSep, noSponsor, ...props }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const contactInfo = [
-    {
-      icon: FiMapPin,
-      label: "100 台北市中正區羅斯福路二段9號8樓之三",
-      href:
-        "https://www.google.com/maps/place/100%E8%87%BA%E5%8C%97%E5%B8%82%E4%B8%AD%E6%AD%A3%E5%8D%80%E7%BE%85%E6%96%AF%E7%A6%8F%E8%B7%AF%E4%BA%8C%E6%AE%B59%E8%99%9F",
-      isExternal: true,
-    },
-    { icon: FiPhone, label: "（02）2706-1837", href: "tel:+886227061837" },
-    {
-      icon: FiMail,
-      label: "contact@rethinktw.org",
-      href: "mailto:contact@rethinktw.org",
-    },
-  ];
-
-  const supportLinks = [
-    { label: "捐款支持", href: "/donate/" },
-    { label: "加入志工", href: "https://rethinktw.cc/volunteer", isExternal: true },
-    { label: "合作提案", href: "https://rethinktw.cc/5Y9hr", isExternal: true },
-    { label: "常見問題", href: "https://rethinktw.cc/refaq", isExternal: true },
-  ];
+  const [subscribeName, setSubscribeName] = useState("");
+  const [subscribeEmail, setSubscribeEmail] = useState("");
 
   const socialLinks = [
     {
       icon: FiFacebook,
       label: "Facebook",
-      href: "https://www.facebook.com/rethink.tw",
+      href: "https://rethinktw.cc/ExwKe",
+      bg: "#0765FE",
     },
     {
       icon: FiInstagram,
       label: "Instagram",
-      href: "https://www.instagram.com/rethink.tw/",
+      href: "https://rethinktw.cc/UmAAR",
+      bg: "#53beee",
     },
     {
-      icon: FiExternalLink,
-      label: "RE:THINK 官網",
-      href: "https://rethinktw.org/",
+      icon: SiThreads,
+      label: "Threads",
+      href: "https://rethinktw.cc/sgI0b",
+      bg: "#2a2a2a",
+    },
+    {
+      icon: FiYoutube,
+      label: "Youtube",
+      href: "https://rethinktw.cc/AxMUh",
+      bg: "red",
+    },
+    {
+      icon: SiLine,
+      label: "Line",
+      href: "https://rethinktw.cc/fNvVc",
+      bg: "#00c300",
     },
   ];
+
+  const websiteLinks = [
+    { label: "關於我們", href: "https://rethinktw.cc/a8fyy", isExternal: true },
+    {
+      label: "必懂的回收知識",
+      href: "https://rethinktw.cc/ZL2W5",
+      isExternal: true,
+    },
+    { label: "課程申請", href: "https://rethinktw.cc/0PDKE", isExternal: true },
+    { label: "企業合作", href: "https://rethinktw.cc/vweYW", isExternal: true },
+    {
+      label: "RE-THINK官網",
+      href: "https://rethinktw.cc/BkzgJ",
+      isExternal: true,
+    },
+    { label: "海廢圖鑑", href: "https://rethinktw.cc/kRoiM", isExternal: true },
+  ];
+
+  const handleSubscribe = (e) => {
+    e.preventDefault();
+    // 实现订阅逻辑
+    console.log("Subscribe:", subscribeName, subscribeEmail);
+  };
 
   return (
     <Box bg="white" {...props}>
@@ -87,198 +86,222 @@ const Footer = ({ isAbout, isTrash, noSep, noSponsor, ...props }) => {
           />
         </Container>
       )}
-      <Box bg="black" color="white" mt={noSponsor ? 0 : responsive("0.5em", "1em")}>
-        <Container py={responsive("2.5em", "4em")} fontSize={responsive("1em", "1.125em")}>
+
+      {/* Footer Main */}
+      <Box bg="black" color="white" pt={responsive("3.75em", "3.75em")}>
+        <Container fontSize={responsive("0.875em", "1em")}>
+          {/* Top Section: Logo + Office Info + Social Media */}
           <Stack
-            direction={responsive("column", "row")}
-            spacing={responsive("2em", "3em")}
-            align={responsive("flex-start", "flex-start")}
+            spacing={responsive("1.25em", "1.25em")}
+            align="center"
+            textAlign="center"
+            mb={responsive("2.5em", "2.5em")}
           >
-            <Stack spacing={responsive("0.9em", "1.25em")} flex={responsive("unset", "1.25")}>
-              <Box fontSize={responsive("1.75em", "2.25em")} fontWeight="900" letterSpacing="0.05em">
-                回收大百科
-              </Box>
-              <Box lineHeight="1.9" maxW={responsive("100%", "42em")}>
-                最通用的回收指南，集結政府公告、回收專業與實際處理狀況，邀請你一起共創最完整的回收資訊。
-              </Box>
-              <Flex wrap="wrap" gap={responsive("0.75em", "1em")}>
-                <Button
-                  href="/donate/"
-                  colorScheme="yellow"
-                  bg="yellow.500"
-                  color="black"
-                  _hover={{ bg: "yellow.400" }}
-                  height={responsive("3em", "3.5em")}
-                  minW={responsive("10em", "11em")}
-                  fontSize={responsive("0.875em", "1em")}
-                >
-                  捐款支持
-                </Button>
-                <Button
-                  href="https://rethinktw.cc/volunteer"
-                  isExternal
-                  variant="outline"
-                  colorScheme="whiteAlpha"
-                  borderColor="white"
-                  color="white"
-                  _hover={{ bg: "white", color: "black" }}
-                  height={responsive("3em", "3.5em")}
-                  minW={responsive("10em", "11em")}
-                  fontSize={responsive("0.875em", "1em")}
-                >
-                  加入志工
-                </Button>
-              </Flex>
-            </Stack>
-            <SimpleGrid
-              spacing={responsive("1.5em", "2em")}
-              columns={responsive(1, 1, 3)}
-              flex={responsive("unset", "1")}
-              minW={responsive("100%", "24em")}
+            {/* RE-THINK Logo */}
+            <Box>
+              <Image
+                src="https://recycle.rethinktw.org/test_blog/wp-content/uploads/2025/12/02_version_White-1024x162.png"
+                alt="RE-THINK"
+                width={responsive("160px", "160px")}
+                display="inline-block"
+              />
+            </Box>
+
+            {/* Office Info Title */}
+            <Box
+              fontSize={responsive("1em", "1em")}
+              color="#7e7e7e"
+              fontWeight="normal"
             >
-              <Stack spacing="0.75em">
-                <Box fontWeight="800" letterSpacing="0.05em" fontSize={responsive("1em", "1.125em")}>
-                  聯絡資訊
-                </Box>
-                <Stack spacing="0.5em">
-                  {contactInfo.map(({ icon, label, href, isExternal }) => (
-                    <Link
-                      key={label}
-                      href={href}
-                      isExternal={isExternal}
-                      color="white"
-                      _hover={{ color: "yellow.300" }}
-                      display="inline-flex"
-                      alignItems="center"
-                      gap="0.5em"
-                    >
-                      <Icon as={icon} aria-hidden fontSize="1.2em" color="yellow.400" />
-                      <Box as="span">{label}</Box>
-                    </Link>
-                  ))}
-                </Stack>
-              </Stack>
-              <Stack spacing="0.75em">
-                <Box fontWeight="800" letterSpacing="0.05em" fontSize={responsive("1em", "1.125em")}>
-                  支持與服務
-                </Box>
-                <Stack spacing="0.5em">
-                  {supportLinks.map(({ label, href, isExternal }) => (
-                    <Link
-                      key={label}
-                      to={!isExternal ? href : undefined}
-                      href={isExternal ? href : undefined}
-                      isExternal={isExternal}
-                      color="white"
-                      _hover={{ color: "yellow.300" }}
-                    >
-                      {label}
-                    </Link>
-                  ))}
-                </Stack>
-              </Stack>
-              <Stack spacing="0.75em">
-                <Box fontWeight="800" letterSpacing="0.05em" fontSize={responsive("1em", "1.125em")}>
-                  關注我們
-                </Box>
-                <Stack spacing="0.5em">
-                  {socialLinks.map(({ icon, label, href }) => (
-                    <Link
-                      key={label}
-                      isExternal
-                      href={href}
-                      color="white"
-                      _hover={{ color: "yellow.300" }}
-                      display="inline-flex"
-                      alignItems="center"
-                      gap="0.5em"
-                    >
-                      <Icon as={icon} aria-hidden fontSize="1.2em" color="yellow.400" />
-                      <Box as="span">{label}</Box>
-                    </Link>
-                  ))}
-                </Stack>
-              </Stack>
-            </SimpleGrid>
-          </Stack>
-          <Stack
-            direction={responsive("column", "row")}
-            spacing={responsive("1em", "1.5em")}
-            pt={responsive("2em", "2.5em")}
-            mt={responsive("2em", "2.5em")}
-            borderTop="1px solid"
-            borderColor="whiteAlpha.400"
-            fontSize={responsive("0.8em", "0.95em")}
-            align={responsive("flex-start", "center")}
-            justify={responsive("flex-start", "space-between")}
-          >
-            <Stack spacing="0.25em">
-              <Box fontWeight="800">社團法人台灣重新思考環境教育協會</Box>
-              <Box>公益勸募字號 衛部救字第1141363195號</Box>
-            </Stack>
+              辦公室資訊
+            </Box>
+
+            {/* Contact Details */}
             <Stack
-              direction={responsive("column", "row")}
-              spacing={responsive("0.75em", "1.25em")}
-              align={responsive("flex-start", "center")}
+              spacing="0.35em"
+              fontSize={responsive("0.875em", "0.875em")}
+              lineHeight="1.6"
             >
-              <Box>Design by 山葵組設計</Box>
-              <Link onClick={onOpen} as="button" color="yellow.300">
-                製作說明
-              </Link>
-              <Box>Copyright © {new Date().getFullYear()} RE-THINK</Box>
+              <Box>電話 : 02-27061837</Box>
+              <Box>地址 : 100台北市中正區羅斯福路二段9號8樓之三</Box>
+              <Box>信箱：service@rethinktw.org</Box>
             </Stack>
+
+            {/* Social Media Icons */}
+            <Flex gap="0.75em" pt="0.5em">
+              {socialLinks.map(({ icon, label, href, bg }) => (
+                <Link
+                  key={label}
+                  href={href}
+                  isExternal
+                  display="inline-flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  w="32px"
+                  h="32px"
+                  bg={bg}
+                  borderRadius="6px"
+                  _hover={{ opacity: 0.8 }}
+                  transition="opacity 0.2s"
+                >
+                  <Icon as={icon} fontSize="18px" color="white" />
+                </Link>
+              ))}
+            </Flex>
           </Stack>
+
+          {/* Middle Section: Newsletter + Links + Donation Info */}
+          <SimpleGrid
+            columns={responsive(1, 1, 3)}
+            spacing={responsive("2.5em", "2.5em")}
+            pt={responsive("2.5em", "2.5em")}
+            pb={responsive("2.5em", "2.5em")}
+            alignItems="start"
+          >
+            {/* Newsletter */}
+            <Stack spacing="0.75em">
+              <Box
+                fontSize={responsive("1em", "1em")}
+                color="#7e7e7e"
+                fontWeight="normal"
+              >
+                訂閱電子報
+              </Box>
+              <Box as="form" onSubmit={handleSubscribe}>
+                <Stack spacing="0.625em">
+                  <Input
+                    type="text"
+                    placeholder="輸入您的姓名"
+                    value={subscribeName}
+                    onChange={(e) => setSubscribeName(e.target.value)}
+                    required
+                    bg="white"
+                    color="black"
+                    borderColor="white"
+                    _placeholder={{ color: "gray.500" }}
+                    _hover={{ borderColor: "gray.300" }}
+                    fontSize="0.875em"
+                    height="2.5em"
+                  />
+                  <Input
+                    type="email"
+                    placeholder="輸入您的電子郵件"
+                    value={subscribeEmail}
+                    onChange={(e) => setSubscribeEmail(e.target.value)}
+                    required
+                    bg="white"
+                    color="black"
+                    borderColor="white"
+                    _placeholder={{ color: "gray.500" }}
+                    _hover={{ borderColor: "gray.300" }}
+                    fontSize="0.875em"
+                    height="2.5em"
+                  />
+                  <Button
+                    type="submit"
+                    bg="yellow.400"
+                    color="black"
+                    _hover={{ bg: "yellow.300" }}
+                    width="100%"
+                    fontSize="0.875em"
+                    height="2.5em"
+                    fontWeight="700"
+                  >
+                    立即訂閱
+                  </Button>
+                </Stack>
+              </Box>
+            </Stack>
+
+            {/* Website Links */}
+            <Stack spacing="0.75em">
+              <Box
+                fontSize={responsive("1em", "1em")}
+                color="#7e7e7e"
+                fontWeight="normal"
+              >
+                網站連結
+              </Box>
+              <SimpleGrid columns={2} spacing="0.5em" pt="0.25em">
+                {websiteLinks.map(({ label, href, isExternal }) => (
+                  <Link
+                    key={label}
+                    href={href}
+                    isExternal={isExternal}
+                    color="white"
+                    fontSize="0.875em"
+                    _hover={{ opacity: 0.8 }}
+                  >
+                    {label}
+                  </Link>
+                ))}
+              </SimpleGrid>
+            </Stack>
+
+            {/* Donation Info */}
+            <Stack spacing="0.75em">
+              <Box
+                fontSize={responsive("1em", "1em")}
+                color="#7e7e7e"
+                fontWeight="normal"
+              >
+                立案捐款資訊
+              </Box>
+              <Stack spacing="0.5em" fontSize="0.875em" lineHeight="1.6">
+                <Box>
+                  立案字號 台內團字第1050089249號函
+                  <br />
+                  公益勸募字號 衛部救字第1141361743號
+                </Box>
+                <Box pt="0.5em">
+                  銀行、帳戶匯款：
+                  <br />
+                  戶　　名：社團法人重新思考環境教育協會
+                  <br />
+                  銀行代號：013 國泰世華銀行
+                  <br />
+                  分　　行：南京東路分行
+                  <br />
+                  帳　　號：003-03-500953-1
+                </Box>
+              </Stack>
+            </Stack>
+          </SimpleGrid>
+
+          {/* Bottom Section: Slogan + Logo */}
+          <Box
+            bg="#111"
+            mx={responsive("-20px", "-20px")}
+            px={responsive("20px", "20px")}
+            py={responsive("1.25em", "1.25em")}
+            mt={responsive("2.5em", "2.5em")}
+          >
+            <Flex
+              direction={responsive("column", "row")}
+              justify="space-between"
+              align={responsive("center", "flex-end")}
+              gap={responsive("1.25em", "0")}
+            >
+              <Box
+                fontSize={responsive("1.5em", "1.5em")}
+                fontWeight="700"
+                lineHeight="1.4"
+                color="yellow.400"
+                textAlign={responsive("center", "left")}
+              >
+                讓環保行動
+                <br />
+                成為你我日常
+              </Box>
+              <Image
+                src="https://recycle.rethinktw.org/test_blog/wp-content/uploads/2025/12/1ftp_EnvironmentalPartner_Horizontal_White-1-1024x429.png"
+                alt="1% for the planet"
+                width={responsive("120px", "160px")}
+              />
+            </Flex>
+          </Box>
         </Container>
       </Box>
-      <Modal isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay bg="rgba(255,255,255,0.5)" />
-        <ModalContent
-          border="3px solid"
-          fontSize="1em"
-          rounded={responsive("1em", "1.5em")}
-          maxWidth={responsive("90vw", "50em")}
-        >
-          <ModalHeader
-            pt="2em"
-            fontWeight="900"
-            fontSize={responsive("1.125em", "1.25em")}
-          >
-            這是，最通用的回收指南。
-            <br />
-            也是，需要你一起共創完成的指標。
-          </ModalHeader>
-          <ModalCloseButton
-            bg="black"
-            color="white"
-            width="2em"
-            height="2em"
-            top={responsive("1.25em", "0.75em")}
-            right={responsive("1.25em", "0.75em")}
-            rounded="full"
-            fontSize="1em"
-            _hover={{
-              bg: "black",
-              color: "white",
-            }}
-            _active={{
-              bg: "black",
-              color: "white",
-            }}
-          />
-          <ModalBody lineHeight="1.75" pb="2em" textAlign="justify">
-            因為台灣的回收規範，會與時俱進，也因為地區不同、後端處理設施不同，而尚未有全國統一的標準答案。所以你在《回收大百科》所看到的資訊，已經是彙整了政府公告資訊、回收顧問專業、實際處理狀況等最大的交集。雖然你還是在少數狀況，會碰到規範有差異之處（例如在社區、辦公大樓等），或者清潔隊員不收等狀況，歡迎你
-            <Link.MdLink
-              href="https://s.no8.io/link/channels/ysfjrgOKln"
-              isExternal
-            >
-              回報給回收大百科團隊
-            </Link.MdLink>
-            ，讓我們可以持續更新內容、與政府及產業溝通和改變。邀請你參與，一起為台灣創造最完整的《回收大百科》！
-            <br />
-            （最終更新時間：2024 年 8 月）
-          </ModalBody>
-        </ModalContent>
-      </Modal>
     </Box>
   );
 };
