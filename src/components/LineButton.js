3b1e7f786b1045fe32b7a9a9fabb2de7383660b8
Renddi Yeh <renddi@relab.cc>
2026-03-17 15:46:42 +0800

    feat: update LineButton component to use responsive size for image and adjust bottom positioning

--- src/components/LineButton.js
+++ src/components/LineButton.js
@@ -2,10 +2,11 @@ import { Box, Image, Link } from "@chakra-ui/react";
 import { responsive } from "./ThemeProvider/theme";
 
 const LineButton = () => {
+  const size = responsive("3em", "4em");
   return (
-    <Box pos="fixed" right={3} bottom={responsive(4, '9em')} zIndex={99}>
+    <Box pos="fixed" right={3} bottom={responsive(4, '8em')} zIndex={99}>
       <Link href="https://s.no8.io/link/channels/ysfjrgOKln" isExternal>
-        <Image src="/line.svg" alt="LINE" w="4em" h="4em" />
+        <Image src="/line.svg" alt="LINE" w={size} h={size} />
       </Link>
     </Box>
   );
